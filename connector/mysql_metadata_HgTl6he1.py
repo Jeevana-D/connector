9e1404973d68a41c7c919f59f843901a4d7071a5
Jeevana-D <83423578+Jeevana-D@users.noreply.github.com>
2024-05-07 23:42:36 +0530

    Delete connector directory

--- connector/mysql_metadata_HgTl6he1.py
+++
@@ -1,9 +0,0 @@
-"""
-This file has been generated from dag_runner.j2
-"""
-from airflow import DAG
-from openmetadata_managed_apis.workflows import workflow_factory
-
-workflow = workflow_factory.WorkflowFactory.create("/opt/airflow/dag_generated_configs/mysql_metadata_HgTl6he1.json")
-workflow.generate_dag(globals())
-dag = workflow.get_dag()
